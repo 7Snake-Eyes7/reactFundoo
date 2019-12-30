5a2003f8c5b78b9511413a1044331edc68bdd47b
Rakshak D S <wasdlifeqecrlove@gmail.com>
2019-12-30 17:35:39 +0530

    [rak] code refactored & readability eased for User & Note Service Implementations

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
-import './App.css';
-import Registration from './Pages/registerPage';
-import Login from './Pages/loginPage';
-import ForgotPassword from './Components/ForgotPassword';
-import ResetPassword from './Components/ResetPassword';
-import AppBar from './Components/Dashboard/Appbar';
-import RegistrationSuccess from './Components/RegistrationSuccess';
-import Verify from './Components/Verify';
-import Note from './Components/Dashboard/Note';
-import dashboard from './Pages/dashboard';
-import archive from './Components/Dashboard/archive';
-import deletNote from './Components/Dashboard/deletNote';
-import deletepage from './Pages/deletepage';
-import UnArchieve from './Pages/unArchivePage';
-import DisplayLabelNotes from './Components/Dashboard/displayLabelNotes';
-import Label from './Pages/label';
-import remainder from './Pages/remainder';
-import  NoteSearchPage  from './Pages/noteSearchPage'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Registration from "./Pages/registerPage";
+import Login from "./Pages/loginPage";
+import ForgotPassword from "./Components/ForgotPassword";
+import ResetPassword from "./Components/ResetPassword";
+import AppBar from "./Components/Dashboard/Appbar";
+import RegistrationSuccess from "./Components/RegistrationSuccess";
+import Verify from "./Components/Verify";
+import Note from "./Components/Dashboard/Note";
+import dashboard from "./Pages/dashboard";
+import archive from "./Components/Dashboard/archive";
+import deletNote from "./Components/Dashboard/deletNote";
+import deletepage from "./Pages/deletepage";
+import UnArchieve from "./Pages/unArchivePage";
+import DisplayLabelNotes from "./Components/Dashboard/displayLabelNotes";
+import Label from "./Pages/label";
+import remainder from "./Pages/remainder";
+import NoteSearchPage from "./Pages/noteSearchPage";
 class App extends Component {
   render() {
     return (
-      
       <Router>
         Label
         <Route path="/" exact component={Login}></Route>
         <Route path="/home" component={dashboard}></Route>
-        <Route path="/labelnotes/:labelForNote" component={Label} ></Route>
+        <Route path="/labelnotes/:labelForNote" component={Label}></Route>
         <Route path="/unarchive" component={UnArchieve}></Route>
         <Route path="/registration" component={Registration}></Route>
-        
         {/* <Route path="/labelnote" component={Label}></Route> */}
-
         <Route path="/forgotpassword" component={ForgotPassword}></Route>
         <Route path="/resetpassword/:token" component={ResetPassword}></Route>
         <Route path="/delete" component={deletepage}></Route>
@@ -39,7 +36,6 @@ class App extends Component {
         <Route path="/verify/:token" component={Verify}></Route>
         <Route path="/remainder" component={remainder}></Route>
         <Route path="/search" component={NoteSearchPage}></Route>
-      
       </Router>
     );
   }
--- src/Controller/Usercontroller.jsx
+++ src/Controller/Usercontroller.jsx
@@ -1,67 +1,81 @@
+import axios from "axios";
 
-import axios from 'axios';
-
-const headers={
-    'Content-Type': 'application/json',
-    'token':localStorage.getItem('token')
-}
+const headers = {
+  "Content-Type": "application/json",
+  token: localStorage.getItem("token")
+};
 
 export function Usercontroller(userRegister) {
-    const baseUrl = process.env.REACT_APP_BASE_URL;
-    let header = {
-        'Content-Type': 'application/json'
-    }
-    return axios.post("http://localhost:9020/" + 'user/registration', userRegister, { header: header });
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  let header = {
+    "Content-Type": "application/json"
+  };
+  return axios.post(
+    "http://localhost:9020/" + "user/registration",
+    userRegister,
+    { header: header }
+  );
 }
-
-
-
 
 export function userLogin(userLogin) {
-    const baseUrl = process.env.BASE_URL;
+  const baseUrl = process.env.BASE_URL;
 
-    let header = {
-        'Content-Type': 'application/json'
-    }
-    return axios.post("http://localhost:9020/" + 'user/login', userLogin, { header: header });
+  let header = {
+    "Content-Type": "application/json"
+  };
+  return axios.post("http://localhost:9020/" + "user/login", userLogin, {
+    header: header
+  });
 }
 export function emailVerify(userEmailVerify) {
-    const baseUrl = process.env.BASE_URL;
+  const baseUrl = process.env.BASE_URL;
 
-    let header = {
-        'Content-Type': 'application/json'
-    }
-    return axios.post("http://localhost:9020/" + 'user/forgotpassword', userEmailVerify, { header: header });
+  let header = {
+    "Content-Type": "application/json"
+  };
+  return axios.post(
+    "http://localhost:9020/" + "user/forgotpassword",
+    userEmailVerify,
+    { header: header }
+  );
 }
 
-export function userPasswordReset(userPassword,userToken) {
-    const baseUrl = process.env.BASE_URL;
-    let token=userToken;
-    console.log(token)
+export function userPasswordReset(userPassword, userToken) {
+  const baseUrl = process.env.BASE_URL;
+  let token = userToken;
+  console.log(token);
 
-    let header = {
-        'Content-Type': 'application/json'
-    }
-    return axios.put('http://localhost:9020/user/resetpassword/'+token, userPassword, { header: header });
+  let header = {
+    "Content-Type": "application/json"
+  };
+  return axios.put(
+    "http://localhost:9020/user/resetpassword/" + token,
+    userPassword,
+    { header: header }
+  );
 }
 
 export function userVerify(userToken) {
-    const baseUrl = process.env.BASE_URL;
-    let token=userToken;
-    console.log(token)
+  const baseUrl = process.env.BASE_URL;
+  let token = userToken;
+  console.log(token);
 
-    let header = {
-        'Content-Type': 'application/json'
-    }
-    
-    return axios.put('http://localhost:9020/user/verify/'+token, null,{ header: header });  
+  let header = {
+    "Content-Type": "application/json"
+  };
+
+  return axios.get("http://localhost:9020/user/verify/" + token, null, {
+    header: header
+  });
 }
 
-export function getLoggedUser(){
-    return axios.get('http://localhost:9020/user/loggedinuser',{ headers : headers });
+export function getLoggedUser() {
+  return axios.get("http://localhost:9020/user/loggedinuser", {
+    headers: headers
+  });
 }
 
-export function uploadPic(formdata){
-    console.log("in userController",formdata.get('pic'))
-    return axios.post('http://localhost:9020/',formdata);
+export function uploadPic(formdata) {
+  console.log("in userController", formdata.get("pic"));
+  return axios.post("http://localhost:9020/", formdata);
 }
